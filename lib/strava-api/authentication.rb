d1a8bb35bc93e656bd40b04c295f81ec95465232
Andrew Olson <anolson@gmail.com>
2011-07-05 09:44:58 -0400

    Removing commented code.

--- lib/strava-api/authentication.rb
+++ lib/strava-api/authentication.rb
@@ -1,33 +1,9 @@
 module StravaApi
   module Authentication
-    
-    # def rides(options = {})
-    #   options_map = {
-    #     :club_id => 'clubId',
-    #     :athlete_id => 'athleteId',
-    #     :athlete_name => 'athleteName',
-    #     :start_date => 'startDate',
-    #     :end_date => 'endDate',
-    #     :start_id => 'startId',
-    #     :offset => 'offset'
-    #   }
-    #   
-    #   #convert between rails format names and camel case
-    #   filtered_options = {}
-    #   options_map.each_pair do |key, converted|
-    #     filtered_options[converted] = options[key] if options[key]
-    #   end
-    # 
-    #   raise StravaApi::CommandError if filtered_options.empty?
-    # 
-    #   result = call("rides", "rides", filtered_options)
-    #   result["rides"].collect {|item| Ride.new(self, item)}
-    # end
-    
+    # authenticate a user with their Strava credentials.
     def login(email, password)
       result = call("authentication/login", nil, {:email => email, :password => password}, :post)
       User.new(self, result)
     end
-    
   end
 end
